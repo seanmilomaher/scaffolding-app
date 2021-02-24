55e16e139c7e13e602f38476ef001cbaea5bf7f0
sean <maherseanmilo@gmail.com>
2021-02-24 11:26:38 -0500

    adds logic to index controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  attr_accessor :content, :name, :title
+ 
+  validates :name,  :presence => true
+  validates :title, :presence => true, :length => { :minimum => 5 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :posts
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
